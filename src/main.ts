b3b2c25c4a2b2c3934a3c250bf57adf0c0b4c7c3
angelo <angelopiletti@gmail.com>
2023-05-12 19:06:55 +0200

    refactor: :truck: alias instead of relative paths

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import './assets/main.css'
+import '@/assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import App from '@/App.vue'
 
 const app = createApp(App)
 
