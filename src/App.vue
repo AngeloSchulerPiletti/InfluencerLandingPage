5fb73968d6897527da162450f036af1916584f4a
angelo <angelopiletti@gmail.com>
2023-05-14 20:14:53 +0200

    fix: :truck: fixed import

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import AdvisorySection from '@/components/sections/AdvisorySection.vue'
-import type ContactSection from '@/components/sections/ContactSection.vue'
+import ContactSection from '@/components/sections/ContactSection.vue'
 import ContentSection from '@/components/sections/ContentSection.vue'
 import IntroSection from '@/components/sections/IntroSection.vue'
-import type MembershipSection from '@/components/sections/MembershipSection.vue'
-import type SupportSection from '@/components/sections/SupportSection.vue'
+import MembershipSection from '@/components/sections/MembershipSection.vue'
+import SupportSection from '@/components/sections/SupportSection.vue'
 </script>
 
 <template>
