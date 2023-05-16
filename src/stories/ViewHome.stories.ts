110b5140c5b3586dc5d281c285d5d4ae3e421755
angelo <angelopiletti@gmail.com>
2023-05-16 19:16:02 +0200

    test: story for view home

---
+++ src/stories/ViewHome.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta } from '@storybook/vue3'
+import ViewHome, { type Props } from '@/components/ViewHome.vue'
+import type { StoryObjProped } from '@/types/storybook.types'
+import { SectionNavigationMock } from '@/stories/mock/sectionNavigation.mock'
+import { SectionIntroMock } from '@/stories/mock/sectionIntro.mock'
+
+const meta: Meta<typeof ViewHome> = {
+  title: 'Views/Home',
+  component: ViewHome,
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
+
+export default meta
+
+type Story = StoryObjProped<typeof ViewHome, Props>
+
+export const Home: Story = {
+  args: {
+    navigation: SectionNavigationMock,
+    intro: SectionIntroMock,
+  },
+}
