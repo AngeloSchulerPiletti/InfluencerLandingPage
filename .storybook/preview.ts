64d546059cb5b47fe00c2d1b3917d62b2fa53a10
angelo <angelopiletti@gmail.com>
2023-05-16 19:02:49 +0200

    test: :wrench: routing support and global styles import

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,12 @@
-import type { Preview } from '@storybook/vue3'
-import '@/assets/style/index.scss'
+import { type Preview, setup } from '@storybook/vue3'
+import { App } from 'vue'
+import '@/assets/scss/index.scss'
+//@ts-ignore
+import { router } from '@/routes'
+
+setup((app: App) => {
+  app.use(router)
+})
 
 const preview: Preview = {
   parameters: {
