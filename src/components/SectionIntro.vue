c2524d7b391f1678ae7fba42ca0ee37f5ea282d7
angelo <angelopiletti@gmail.com>
2023-05-16 19:15:08 +0200

    feat: :sparkles: add section intro

---
+++ src/components/SectionIntro.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import SgTextContent, {
+  type Props as TextContentProps,
+} from '@/components/SgTextContent.vue'
+import SgCarousel, {
+  type Props as CarouselProps,
+} from '@/components/SgCarousel/SgCarousel.vue'
+
+export interface Props {
+  textContent: TextContentProps
+  carousel: CarouselProps
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-10 py-7">
+    <SgTextContent v-bind="textContent" />
+    <SgCarousel v-bind="carousel" />
+  </div>
+</template>
