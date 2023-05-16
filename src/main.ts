825c25005a44c0a4b60a3a3021893dc38961b51b
angelo <angelopiletti@gmail.com>
2023-05-16 19:04:57 +0200

    feat: :construction: route support and global styles

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import '@/assets/main.css'
-
+import '@/assets/scss/index.scss'
+import { router } from '@/routes'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from '@/App.vue'
 
 const app = createApp(App)
 
+app.use(router)
 app.use(createPinia())
 
 app.mount('#app')
