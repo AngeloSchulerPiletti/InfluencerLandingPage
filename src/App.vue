4f138e32737058606eb9324bb043b835f54ec791
angelo <angelopiletti@gmail.com>
2023-05-16 19:09:05 +0200

    feat: implement router view

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
-<script setup lang="ts">
-import AdvisorySection from '@/components/sections/AdvisorySection.vue'
-import ContactSection from '@/components/sections/ContactSection.vue'
-import ContentSection from '@/components/sections/ContentSection.vue'
-import IntroSection from '@/components/sections/IntroSection.vue'
-import MembershipSection from '@/components/sections/MembershipSection.vue'
-import SupportSection from '@/components/sections/SupportSection.vue'
+<script lang="ts" setup>
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <main>
-    <IntroSection />
-    <ContentSection />
-    <AdvisorySection />
-    <MembershipSection />
-    <SupportSection />
-    <ContactSection />
-  </main>
+  <router-view />
 </template>
