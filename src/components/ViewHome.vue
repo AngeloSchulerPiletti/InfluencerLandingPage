43e9d984a72d9129180e214935045504c67ed258
angelo <angelopiletti@gmail.com>
2023-05-16 19:15:48 +0200

    feat: Add view home

---
+++ src/components/ViewHome.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import SectionNavigation, {
+  type Props as NavigationProps,
+} from '@/components/SectionNavigation.vue'
+import SectionIntro, {
+  type Props as IntroProps,
+} from '@/components/SectionIntro.vue'
+
+export interface Props {
+  navigation: NavigationProps
+  intro: IntroProps
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    <SectionNavigation v-bind="navigation" />
+  </div>
+  <main>
+    <SectionIntro v-bind="intro" />
+  </main>
+</template>
