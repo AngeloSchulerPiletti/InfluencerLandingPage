e0a22ac94db55c9efd95b944b39e52e0234fbb43
angelo <angelopiletti@gmail.com>
2023-05-16 19:14:08 +0200

    feat: :sparkles: add text content component

---
+++ src/components/SgTextContent.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import SgLink, { type SlotedProps as LinkProps } from '@/components/SgLink.vue'
+
+export interface Props {
+  title: string
+  paragraph: string
+  alignment?: 'left' | 'center' | 'right'
+  link?: LinkProps
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  alignment: 'left',
+})
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <h2>{{ title }}</h2>
+    <p>{{ paragraph }}</p>
+    <SgLink
+      v-if="link"
+      :to="link.to"
+      apply-styles
+      variant="fill"
+    >
+      {{ link.title }}
+    </SgLink>
+  </div>
+</template>
