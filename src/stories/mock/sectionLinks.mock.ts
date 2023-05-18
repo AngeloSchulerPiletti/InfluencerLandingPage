36f61a5f4d2f27a53b596194d75f92dd747dc882
angelo <angelopiletti@gmail.com>
2023-05-18 14:24:53 +0200

    test: :clown_face: mock for section links

---
+++ src/stories/mock/sectionLinks.mock.ts
@@ -0,0 +1,8 @@
+import { type Props } from '@/components/SectionLinks.vue'
+import { SgLinkListDarkMock } from '@/stories/mock/sgLinkList.mock'
+import { SgTextContentLeftDarkMock } from '@/stories/mock/sgTextContent.mock'
+
+export const SectionLinksMock: Props = {
+  linkList: SgLinkListDarkMock,
+  textContent: SgTextContentLeftDarkMock,
+}
