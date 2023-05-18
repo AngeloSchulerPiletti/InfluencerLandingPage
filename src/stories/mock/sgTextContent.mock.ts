6dff9f04c6dbd5fb302b6327098eb45bf8656bda
angelo <angelopiletti@gmail.com>
2023-05-18 12:00:02 +0200

    test: :white_check_mark: refactor story and add dark mode

--- src/stories/mock/sgTextContent.mock.ts
+++ src/stories/mock/sgTextContent.mock.ts
@@ -1,38 +1,39 @@
 import { type Props as TextContentProps } from '@/components/SgTextContent.vue'
 import { faker } from '@faker-js/faker'
+import { type SlotedProps as LinkProps } from '@/components/SgLink.vue'
+
+const getLink = (): LinkProps => ({
+  title: faker.lorem.words({ min: 1, max: 3 }),
+  to: 'https://google.com',
+  applyStyles: true,
+  variant: 'fill',
+})
 
 export const SgTextContentLeftMock: TextContentProps = {
-  paragraph: faker.lorem.words({ min: 3, max: 30 }),
+  paragraphs: [faker.lorem.words({ min: 3, max: 30 })],
   title: faker.lorem.words({ min: 1, max: 5 }),
   alignment: 'left',
-  link: {
-    title: faker.lorem.words({ min: 1, max: 3 }),
-    to: 'https://google.com',
-    applyStyles: true,
-    variant: 'fill',
-  },
+  link: getLink(),
 }
 
 export const SgTextContentCenterMock: TextContentProps = {
-  paragraph: faker.lorem.words({ min: 3, max: 30 }),
+  paragraphs: [faker.lorem.words({ min: 3, max: 30 })],
   title: faker.lorem.words({ min: 1, max: 5 }),
   alignment: 'center',
-  link: {
-    title: faker.lorem.words({ min: 1, max: 3 }),
-    to: 'https://google.com',
-    applyStyles: true,
-    variant: 'fill',
-  },
+  link: getLink(),
 }
 
 export const SgTextContentRightMock: TextContentProps = {
-  paragraph: faker.lorem.words({ min: 3, max: 30 }),
+  paragraphs: [faker.lorem.words({ min: 3, max: 30 })],
   title: faker.lorem.words({ min: 1, max: 5 }),
   alignment: 'right',
-  link: {
-    title: faker.lorem.words({ min: 1, max: 3 }),
-    to: 'https://google.com',
-    applyStyles: true,
-    variant: 'fill',
-  },
+  link: getLink(),
+}
+
+export const SgTextContentLeftDarkMock: TextContentProps = {
+  paragraphs: [faker.lorem.words({ min: 3, max: 30 })],
+  title: faker.lorem.words({ min: 1, max: 5 }),
+  alignment: 'left',
+  link: getLink(),
+  darkMode: true
 }
