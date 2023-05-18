ecd03b39767d1ee8d4958412c23318722b259fc3
angelo <angelopiletti@gmail.com>
2023-05-18 16:40:44 +0200

    test: :clown_face: add helper for getting images

--- src/stories/mock/helper.ts
+++ src/stories/mock/helper.ts
@@ -1,3 +1,4 @@
+import type { Image } from '@/types/media.types'
 import { faker } from '@faker-js/faker'
 
 const icons = [
@@ -15,3 +16,8 @@ export const getRandomIconName = () => {
   const int = faker.number.int({ min: 0, max: 6 })
   return icons[int]
 }
+
+export const getImage = (): Image => ({
+  url: faker.image.url(),
+  alt: faker.lorem.words(5),
+})
