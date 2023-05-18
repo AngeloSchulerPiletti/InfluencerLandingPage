81b2e14dac79e52378b165c9d10bdbf26483ec38
angelo <angelopiletti@gmail.com>
2023-05-18 14:24:34 +0200

    feat: :sparkles: create section links

---
+++ src/components/SectionLinks.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import SgLinkList, {
+  type Props as LinkListProps,
+} from '@/components/SgLinkList.vue'
+import SgTextContent, {
+  type Props as TextContentProps,
+} from '@/components/SgTextContent.vue'
+
+export interface Props {
+  textContent: TextContentProps
+  linkList: LinkListProps
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-8 bg-secondary-900 py-8">
+    <div>
+      <SgTextContent v-bind="textContent" />
+    </div>
+    <div>
+      <SgLinkList v-bind="linkList" />
+    </div>
+  </div>
+</template>
