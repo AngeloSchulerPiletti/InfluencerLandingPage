69324979a7dd2fa5957e04afe3781995bc6eb11e
angelo <angelopiletti@gmail.com>
2023-05-18 12:22:57 +0200

    test: creat mock for content cente

---
+++ src/stories/mock/sectionContentCenter.mock.ts
@@ -0,0 +1,6 @@
+import { type Props as ContentCenterProps } from '@/components/SectionContentCenter.vue'
+import { SgTextContentCenterMock } from '@/stories/mock/sgTextContent.mock'
+
+export const SectionContentCenterMock: ContentCenterProps = {
+  textContent: SgTextContentCenterMock,
+}
